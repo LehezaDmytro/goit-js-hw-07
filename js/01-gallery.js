0b9feec16c58a21f9b5c2d6a1c5a32f8e746525f
Leheza Dmytro <legeza.dmitriy@icloud.com>
2023-01-07 17:17:38 +0200

    Виконано всі завдання із ДЗ №7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery')
 
-console.log(galleryItems);
+galleryItems.forEach(({preview, original, description}) => {
+    gallery.insertAdjacentHTML('beforeend', 
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>    
+    </div>`)
+})
+
+gallery.addEventListener('click', clickOnThePicture)
+
+function clickOnThePicture(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return
+    }
+    
+    const instance = basicLightbox.create(`
+	<img src="${event.target.dataset.source}">
+    `);
+    instance.show();
+
+    document.addEventListener('keydown', closeFullSizeImage)
+    function closeFullSizeImage(event) {
+    if (event.code === "Escape")
+        instance.close();
+    }
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+galleryItems.forEach(({ preview, original, description }) => {
+    gallery.insertAdjacentHTML('beforeend',
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`)
+});
+
+const clickOnThePicture = new SimpleLightbox('.gallery__item', { captionDelay: 250, captionsData: 'alt'});
+clickOnThePicture.on('show.simplelightbox', function () {});
+
